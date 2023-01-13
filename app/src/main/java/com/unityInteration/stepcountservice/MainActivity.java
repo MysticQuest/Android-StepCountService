9688a9f0a3af8706e41c9aafa4c53671c69d9ea7
Mystic Quest <l1skou@gmail.com>
2023-01-13 18:08:22 +0200

    adds listener for sensor

--- app/src/main/java/com/unityInteration/stepcountservice/MainActivity.java
+++ app/src/main/java/com/unityInteration/stepcountservice/MainActivity.java
@@ -1,6 +1,7 @@
 package com.unityInteration.stepcountservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent serviceIntent = new Intent(this, StepCounterService.class);
+        startService(serviceIntent);
     }
 }
