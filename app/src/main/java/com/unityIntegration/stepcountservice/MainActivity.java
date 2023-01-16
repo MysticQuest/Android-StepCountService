737d2e0b25db562da1544a418729bda2a1ba4bb4
Mystic Quest <l1skou@gmail.com>
2023-01-16 19:18:23 +0200

    changes package into a library, updates manifest/build files

--- app/src/main/java/com/unityIntegration/stepcountservice/MainActivity.java
+++ app/src/main/java/com/unityIntegration/stepcountservice/MainActivity.java
@@ -1,18 +1,18 @@
-package com.unityIntegration.stepcountservice;
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent serviceIntent = new Intent(this, StepCounterService.class);
-        startService(serviceIntent);
-    }
-}
+//package com.unityIntegration.stepcountservice;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import android.content.Intent;
+//
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        Intent serviceIntent = new Intent(this, StepCounterService.class);
+//        startService(serviceIntent);
+//    }
+//}
